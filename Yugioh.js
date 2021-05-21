c3aa718967085ab9905bc5d598a23f390e65f8c5
Matthew Adesina <57835016+Himole@users.noreply.github.com>
2021-05-21 20:13:57 +0100

    Update Yugioh.js

--- Yugioh.js
+++ Yugioh.js
@@ -1,1 +1,51 @@
+function checkYuGiOh(n) {
+    if(!Number(n)){
+        let result = JSON.stringify(n);
+        return `invalid parameter: "${result}"`;
+    }
 
+    else{
+        let checkDivisible = [];
+
+        for (let i=1; i<=n; i++){
+            checkDivisible.push(i);
+        }
+    
+
+        for (let i=1; i<=checkDivisible.length; i++){
+        
+            if (checkDivisible[i]%2 == 0){
+                if(checkDivisible[i]%3 == 0 && checkDivisible[i]%5 == 0){
+                    checkDivisible[i] = "yu-gi-oh";
+                }
+                else if(checkDivisible[i]%3 == 0){
+                    checkDivisible[i] = "yu-gi";
+                }
+                else if(checkDivisible[i]%5 == 0){
+                    checkDivisible[i] = "yu-oh";
+                }
+                else{
+                    checkDivisible[i] = "yu";
+                }
+            }
+
+            else if (checkDivisible[i]%3 == 0){
+
+                if(checkDivisible[i]%5 == 0 ){
+                    checkDivisible[i] = "gi-oh";
+                }
+                else {
+                    checkDivisible[i] = "gi";
+                }
+            }
+
+            else if (checkDivisible[i]%5 == 0){
+                checkDivisible[i] = "oh";
+            }
+        
+        };
+    return checkDivisible;
+    };
+}
+
+console.log(checkYuGiOh('5'));
