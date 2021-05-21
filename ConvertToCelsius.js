54edd5572a8ef2bb73303c5fdc3789ee88e6109c
Matthew Adesina <57835016+Himole@users.noreply.github.com>
2021-05-21 19:47:20 +0100

    Update ConvertToCelsius.js

--- ConvertToCelsius.js
+++ ConvertToCelsius.js
@@ -1,1 +1,32 @@
+function convertFarhToCelsius(farh){
+    let Cels = (farh - 32)*(5/9);
+    let result;
 
+    if (typeof(farh) == "number"){
+        result =  Cels.toFixed(4);
+    }
+
+    else if(typeof(farh) == "string"){
+        let number = parseInt(farh);
+        if( number === "number"){
+            result = (number - 32)*(5/9);
+        } 
+    }
+
+    else if (typeof(farh) == Array.isArray){
+        result = `[${farh}] is not a valid number but a/an Array`;
+    }
+
+    else if (typeof(farh) == Object){
+        result = `${farh} is not a valid number but a/an ${(typeof(farh))}`;
+    }
+
+    else{
+        result = `${farh} is not a valid number but a/an ${typeof(farh)}`;
+    }
+    
+
+return result;
+}
+
+console.log(convertFarhToCelsius({1: 'one', 2: 'two', 3: 'three'}));
